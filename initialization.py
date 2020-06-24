ae33776ffd5bb8ec37837211b7e7b2c03b485dc8
FanKuan44 <49996342+FanKuan44@users.noreply.github.com>
2020-06-24 14:25:56 +0700

    Add files via upload

--- initialization.py
+++ initialization.py
@@ -5,13 +5,13 @@ import math
 from shape import  drawObject
 
 class Init_Global_Para(drawObject):
-    def __init__(self, typeDraw="solids"):
+    def __init__(self, typeDraw="face"):
         drawObject.__init__(self)
         self.isTexture = 0
-        self.typeDraw = typeDraw #points,lines,solids
+        self.typeDraw = typeDraw # points, lines, face
 
         ###  TOGGLE DRAW DISPLAYS  ###
-        self.toggleAxes = 1
+        self.toggleAxes = False
         self.toggleParams = 1
 
         ###  Begin PROJECTION  ###
@@ -38,7 +38,7 @@ class Init_Global_Para(drawObject):
         ###  End PROJECTION  ###
 
         ###  LIGHTING  ###
-        self.toggleLight = 1
+        self.toggleLight = True
         # self.distance = 5
         self.lightPosX = 0
         self.lightPosY = 0
@@ -90,7 +90,6 @@ class Init_Global_Para(drawObject):
         # self.drawLight()
 
 
-
     def redisplayAll(self):
         self.displayReshape(1024, 768)
         glutPostRedisplay()
@@ -118,28 +117,26 @@ class Init_Global_Para(drawObject):
             ###  Label axes ###
             glColor3fv(self.white)
             glRasterPos3d(len,0,0)
-            self.Print("X")
             glRasterPos3d(0,len,0)
-            self.Print("Y")
             glRasterPos3d(0,0,len)
-            self.Print("Z")
-            if (self.toggleLight):
+            if self.toggleLight:
                 glEnable(GL_LIGHTING)
 
     def drawLight(self):
         ###  Light switch ###
-        if (self.toggleLight):
+        if self.toggleLight:
             ###  Translate intensity to color vectors ###
             Ambient    =[0.01*self.ambient ,0.01*self.ambient ,0.01*self.ambient ,1.0]
             Diffuse    =[0.01*self.diffuse ,0.01*self.diffuse ,0.01*self.diffuse ,1.0]
             Specular   =[0.01*self.specular,0.01*self.specular,0.01*self.specular,1.0]
-            Position   =[self.lightPosX,self.lightPosY,0.0,1.0]
+            # Position  = [self.lightPosX, self.lightPosY, 0.0, 1.0]
+            Position   = [0, 0, 5, 1.0]
             ###  Draw light position as sphere (still no lighting here) ###
             glPushMatrix()
-            glColor3fv(self.white)
+            glColor3fv([1,0,0])
             glDisable(GL_LIGHTING)
-            glTranslate(Position[0],Position[1],Position[2])
-            self.make_sphere(0.3)
+            glTranslate(Position[0], Position[1], Position[2])
+            # self.make_sphere(0.3)
             glPopMatrix()
             ###  Set ambient, diffuse, specular components and position of light 0 ###
             glLightfv(GL_LIGHT0,GL_AMBIENT, Ambient)
@@ -157,12 +154,3 @@ class Init_Global_Para(drawObject):
             glEnable(GL_LIGHT0)
         else:
             glDisable(GL_LIGHTING)
-
-    def PrintAt(self, x, y, *args):
-        glWindowPos2i(x,y)
-        self.Print(*args)
-
-    def Print(self, *args):
-        for ia in args:
-            for ch in ia:
-                glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18 , ctypes.c_int( ord(ch)))
--- test3.py
+++ test3.py
@@ -8,54 +8,51 @@ from pyopengltk import OpenGLFrame
 from initialization import Init_Global_Para
 from tkinter import *
 
-class Draw(Init_Global_Para,OpenGLFrame):
-    def __init__(self, shapeObject, isTexture=None, typeDraw='solids',*args, **kw):
+class Draw(Init_Global_Para, OpenGLFrame):
+    def __init__(self, shapeObject, isTexture=False, isLighting=False, typeDraw='face', *args, **kw):
         Init_Global_Para.__init__(self, typeDraw=typeDraw)
-        OpenGLFrame.__init__(self,*args,**kw)
+        OpenGLFrame.__init__(self, *args, **kw)
         self.object = shapeObject
 
         self.texture = 0 # save texture to draw
-        self.bind('<Triple-Button-2>',self.tkReset)
+        self.bind('<Triple-Button-2>', self.tkReset)
 
         ### Begin APHIN ###
-        self.aphinType = 3 # 0 is Scale; 1 is Rotate; 2 is Translate
+        self.aphinType = -1 # 0 is Scale; 1 is Rotate; 2 is Translate
         self.varScale = (DoubleVar(), DoubleVar(), DoubleVar())
-        self.mouse = (0.0,0.0)
-        self.moveR = (0.0,0.0)
-        self.moveT = (0.0,0.0)
-        self.preMoveR = (0.0,0.0)
-        self.preMoveT = (0.0,0.0)
+        self.mouse = (0.0, 0.0)
+        self.moveR = (0.0, 0.0)
+        self.moveT = (0.0, 0.0)
+        self.preMoveR = (0.0, 0.0)
+        self.preMoveT = (0.0, 0.0)
         self.sizeObject = 0.0
         self.isScaleFirst = 0
         self.isTranslateFirst = 0
         self.isRotateFirst = 0
         self.isLightSourceFist = 0
-        # self.bind('<Button-2>', self.tkSetAphin1)
-        # self.bind('<Double-Button-2>', self.tkSetAphin2)
         self.bind('<Button-1>', self.tkRecordMouse)
-        # self.bind('<Shift-B1-Motion>', self.tkSizeObject)
 
         if self.aphinType in range(4):
             self.bind('<B1-Motion>', self.tkAphin)
         ### End APHIN ###
 
         ### Begin LIGHT ###
-        self.toggleLight = 1
-        self.lightSourceT = (0.0,0.0)
-        self.preLightSourceT = (0.0,0.0)
+        self.toggleLight = isLighting
+        self.lightSourceT = (0.0, 0.0)
+        self.preLightSourceT = (0.0, 0.0)
         self.bind('<Button-3>', self.tkSetAphin3)
         ### End LIGHT ###
     
         
     def tkReset(self, event):
         self.aphinType = 4 # 0 is Scale; 1 is Rotate; 2 is Translate
-        self.mouse = (0.0,0.0)
-        self.moveR = (0.0,0.0)
-        self.moveT = (0.0,0.0)
-        self.lightSourceT = (0.0,0.0)
-        self.preMoveR = (0.0,0.0)
-        self.preMoveT = (0.0,0.0)
-        self.preLightSourceT = (0.0,0.0)
+        self.mouse = (0.0, 0.0)
+        self.moveR = (0.0, 0.0)
+        self.moveT = (0.0, 0.0)
+        self.lightSourceT = (0.0, 0.0)
+        self.preMoveR = (0.0, 0.0)
+        self.preMoveT = (0.0, 0.0)
+        self.preLightSourceT = (0.0, 0.0)
 
     def tkSetAphin1(self, event):
         self.aphinType = 1
@@ -69,24 +66,25 @@ class Draw(Init_Global_Para,OpenGLFrame):
         self.bind('<B1-Motion>', self.tkAphin)
         print(self.aphinType)
 
-    def tkSetAphin3(self,event):
-        # self.toggleLight = 1- self.toggleLight
+    def tkSetAphin3(self, event):
+        # self.toggleLight = 1 - self.toggleLight
         self.aphinType = 3
         self.bind('<B1-Motion>', self.tkAphin)
         print(self.aphinType)
 
     def tkSizeObject(self, event):
-        self.sizeObject = math.sqrt(math.pow((event.x - self.mouse[0]),2)+math.pow((event.y-self.mouse[1]),2))/170
+        self.sizeObject = math.sqrt(math.pow((event.x - self.mouse[0]), 2)+math.pow((event.y-self.mouse[1]), 2))/170
 
     def tkRecordMouse(self, event):
-        if self.aphinType == 1:
-            self.mouse = (event.x - self.preMoveR[0],event.y + self.preMoveR[1])
-        elif self.aphinType == 2:
-            self.mouse = (event.x - self.preMoveT[0],event.y + self.preMoveT[1])
-        elif self.aphinType == 3:
-            self.mouse = (event.x - self.preLightSourceT[0],event.y + self.preLightSourceT[1])
-        else:
+        if self.aphinType == 0:
             self.mouse = (event.x, event.y)
+        elif self.aphinType == 1:
+            self.mouse = (event.x - self.preMoveR[0], event.y + self.preMoveR[1])
+        elif self.aphinType == 2:
+            self.mouse = (event.x - self.preMoveT[0], event.y + self.preMoveT[1])
+        elif self.aphinType == 3:
+            self.mouse = (event.x - self.preLightSourceT[0], event.y + self.preLightSourceT[1])
+
         
     def v3distsq(self, a, b):
         d = (a[0] - b[0], a[1] - b[1], a[2] - b[2])
@@ -102,14 +100,14 @@ class Draw(Init_Global_Para,OpenGLFrame):
         tempX = event.x - self.mouse[0]
         tempY = self.mouse[1] - event.y
         if self.aphinType == 1:
-            self.moveR =  (tempX/6,tempY/6)
-            self.preMoveR = (tempX,tempY)
+            self.moveR =  (tempX/6, tempY/6)
+            self.preMoveR = (tempX, tempY)
         elif self.aphinType == 2:
-            self.moveT =  (tempX * scale,tempY * scale)
-            self.preMoveT = (tempX,tempY)
+            self.moveT =  (tempX * scale, tempY * scale)
+            self.preMoveT = (tempX, tempY)
         elif self.aphinType == 3:
-            self.lightSourceT =  (tempX * scale,tempY * scale)
-            self.preLightSourceT = (tempX,tempY)
+            self.lightSourceT =  (tempX * scale, tempY * scale)
+            self.preLightSourceT = (tempX, tempY)
 
     def Init(self):
         glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -142,100 +140,95 @@ class Draw(Init_Global_Para,OpenGLFrame):
     def redraw(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT)
         if self.aphinType == 3:
-            self.lightPosX, self.lightPosY = self.lightSourceT[0],self.lightSourceT[1]
-            
+            self.lightPosX, self.lightPosY = self.lightSourceT[0], self.lightSourceT[1]
         self.drawLight()
-
         glLoadIdentity()
-        # gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0) #void gluLookAt(GLdouble eyeX,GLdouble eyeY,GLdouble eyeZ,GLdouble centerX,GLdouble centerY,GLdouble centerZ,GLdouble upX,GLdouble upY,GLdouble upZ);
-        # gluLookAt(self.eyeX, self.eyeY, self.eyeZ, self.centerX, self.centerY, self.centerZ, self.upX, self.upY, self.upZ) #void gluLookAt(GLdouble eyeX,GLdouble eyeY,GLdouble eyeZ,GLdouble centerX,GLdouble centerY,GLdouble centerZ,GLdouble upX,GLdouble upY,GLdouble upZ);
         self.displayEye()
         glPushMatrix()
 
         if self.aphinType == 0:
             glScale(self.varScale[0].get(), self.varScale[1].get(), self.varScale[2].get())
+            if self.isTranslateFirst:
+                glTranslatef(self.moveT[0] , self.moveT[1], 0.0)
             if self.isRotateFirst:
                 glRotatef(self.moveR[1], 1, 0, 0)
                 glRotatef(self.moveR[0], 0, 1, 0)
-            if self.isTranslateFirst:
-                glTranslatef(self.moveT[0] , self.moveT[1], 0.0)
             if self.isLightSourceFist:
-                self.lightPosX, self.lightPosY = self.lightSourceT[0],self.lightSourceT[1]
+                self.lightPosX, self.lightPosY = self.lightSourceT[0], self.lightSourceT[1]
         elif self.aphinType == 1:
             if self.isScaleFirst:
                 glScale(self.varScale[0].get(), self.varScale[1].get(), self.varScale[2].get())
-            glRotatef(self.moveR[1], 1, 0, 0)
-            glRotatef(self.moveR[0], 0, 1, 0)
             if self.isTranslateFirst:
                 glTranslatef(self.moveT[0] , self.moveT[1], 0.0)
+            glRotatef(self.moveR[1], 1, 0, 0)
+            glRotatef(self.moveR[0], 0, 1, 0)
             if self.isLightSourceFist:
-                self.lightPosX, self.lightPosY = self.lightSourceT[0],self.lightSourceT[1]
+                self.lightPosX, self.lightPosY = self.lightSourceT[0], self.lightSourceT[1]
         elif self.aphinType == 2:
             if self.isScaleFirst:
                 glScale(self.varScale[0].get(), self.varScale[1].get(), self.varScale[2].get())
+            glTranslatef(self.moveT[0] , self.moveT[1], 0.0)
             if self.isRotateFirst:
                 glRotatef(self.moveR[1], 1, 0, 0)
                 glRotatef(self.moveR[0], 0, 1, 0)
-            glTranslatef(self.moveT[0] , self.moveT[1], 0.0)
             if self.isLightSourceFist:
-                self.lightPosX, self.lightPosY = self.lightSourceT[0],self.lightSourceT[1]
+                self.lightPosX, self.lightPosY = self.lightSourceT[0], self.lightSourceT[1]
         elif self.aphinType == 3:
             if self.isScaleFirst:
                 glScale(self.varScale[0].get(), self.varScale[1].get(), self.varScale[2].get())
+            if self.isTranslateFirst:
+                glTranslatef(self.moveT[0] , self.moveT[1], 0.0)
             if self.isRotateFirst:
                 glRotatef(self.moveR[1], 1, 0, 0)
                 glRotatef(self.moveR[0], 0, 1, 0)   
-            if self.isTranslateFirst:
-                glTranslatef(self.moveT[0] , self.moveT[1], 0.0)
-            self.lightPosX, self.lightPosY = self.lightSourceT[0],self.lightSourceT[1]
+            self.lightPosX, self.lightPosY = self.lightSourceT[0], self.lightSourceT[1]
 
+        glColor3fv([64, 255, 0])
         if self.object == 'cube':
-            self.make_cube(self.sizeObject)
+            self.make_cube(self.sizeObject * 0.3)
         elif self.object == 'box':
-            self.make_box(1, 2, 3)
+            self.make_box(self.sizeObject, self.sizeObject * 2, self.sizeObject * 1.5)
         elif self.object == 'teapot':
             self.make_teapot(self.sizeObject)
         elif self.object == 'sphere':
             self.make_sphere(self.sizeObject)  
-
+        elif self.object == 'cylinder':
+            self.make_cylinder(self.sizeObject * 0.25, self.sizeObject * 3 * 0.25)
+        elif self.object == 'torus':
+            self.make_torus(self.sizeObject * 0.3, self.sizeObject * 3 * 0.3)
+        elif self.object == 'cone':
+            self.make_cone(self.sizeObject * 0.3, self.sizeObject * 2 * 0.3)
         # glRotatef(self.rota, 1.0, 1.0, 1.0)
         # self.make_cube(self.sizeObject)
-        # self.make_box(self.sizeObject,self.sizeObject*2,self.sizeObject*1.5)
+        # self.make_box(self.sizeObject, self.sizeObject*2, self.sizeObject*1.5)
         # self.make_sphere(self.sizeObject)
-        # self.make_cone(self.sizeObject,self.sizeObject*3)
-        # self.make_cylinder(self.sizeObject,self.sizeObject*3)
-        # self.make_torus(self.sizeObject,self.sizeObject*3)
+        # self.make_cone(self.sizeObject, self.sizeObject*3)
+        # self.make_cylinder(self.sizeObject, self.sizeObject*3)
+        # self.make_torus(self.sizeObject, self.sizeObject*3)
         # self.make_teapot(self.sizeObject)
         # self.make_teapot(2)
-        # self.make_truncatedCone(self.sizeObject,self.sizeObject*2,self.sizeObject*2)
-        # self.make_pyramid(self.sizeObject*2,self.sizeObject*3)
+        # self.make_truncatedCone(self.sizeObject, self.sizeObject*2, self.sizeObject*2)
+        # self.make_pyramid(self.sizeObject*2, self.sizeObject*3)
         self.drawAxes()
 
-        # self.PrintAt(5,5,f"View Angle (th, ph) =({self.th}, {self.ph})")
-        # self.PrintAt(5,25,f"toggleProjection =({self.toggleProjection})")
-        # self.PrintAt(5,45,f"toggleProjection_Perspective =({self.toggleProjection_Perspective})")
-        # self.PrintAt(5,65,f"toggleProjection_LookAt_Eye =({self.toggleProjection_LookAt_Eye})")
-        # self.PrintAt(5,85,f"toggleProjection__LookAt_Center =({self.toggleProjection__LookAt_Center})")
-        # self.PrintAt(5,105,f"toggleProjection__LookAt_Up =({self.toggleProjection__LookAt_Up})")
+        # self.PrintAt(5, 5, f"View Angle (th, ph) =({self.th}, {self.ph})")
+        # self.PrintAt(5, 25, f"toggleProjection =({self.toggleProjection})")
+        # self.PrintAt(5, 45, f"toggleProjection_Perspective =({self.toggleProjection_Perspective})")
+        # self.PrintAt(5, 65, f"toggleProjection_LookAt_Eye =({self.toggleProjection_LookAt_Eye})")
+        # self.PrintAt(5, 85, f"toggleProjection__LookAt_Center =({self.toggleProjection__LookAt_Center})")
+        # self.PrintAt(5, 105, f"toggleProjection__LookAt_Up =({self.toggleProjection__LookAt_Up})")
         glPopMatrix()
         glFlush()
 
 
-    def tkResize(self,evt):
+    def tkResize(self, evt):
         self.width, self.height = evt.width, evt.height
         self.asp =  self.width/self.height if self.height > 0 else 1
         if self.winfo_ismapped():
             glViewport(0, 0, self.width, self.height)
             self.initgl()
-
-    # def initgl(self):
-    #     self.Init()
-    #     glMatrixMode(GL_PROJECTION)
-    #     glLoadIdentity()
-    #     gluPerspective(self.fov,self.asp,self.zNear,self.zFar) #void gluPerspective(	GLdouble fovy, GLdouble aspect,GLdouble zNear, 	GLdouble zFar);
-    #     glMatrixMode(GL_MODELVIEW)
-    #     glLoadIdentity()
     
+
     def LoadGLTextures(self):
         self.texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self.texture)
@@ -250,7 +243,6 @@ class Draw(Init_Global_Para,OpenGLFrame):
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.image.width, self.image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, self.img_data)
         # glBindTexture( GL_TEXTURE_2D, 0 )
 
-    # def SetMaterialColor(self,ambient,diff_use):
+    # def SetMaterialColor(self, ambient, diff_use):
     #     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient)
     #     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diff_use)
-
